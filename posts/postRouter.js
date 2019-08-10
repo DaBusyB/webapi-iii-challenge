7c086e9818a29ed98a5cbb539af76539e96a5557
Brandi <Brandi@Brandis-iMac-4.local>
2019-08-10 14:20:07 -0400

    get/id added to user and posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -13,8 +13,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.getById(req.params.id)
+    
+        if(post) {
+            res.status(200).json(post)
+        } else {
+            res.status(404).json({message: 'Post not found'})
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Error retrieving the post'})
+    }
 });
 
 router.delete('/:id', (req, res) => {
--- users/userRouter.js
+++ users/userRouter.js
@@ -22,8 +22,18 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await Users.getById(req.params.id)
+    
+        if(user) {
+            res.status(200).json(user)
+        } else {
+            res.status(404).json({message: 'User not found'})
+        }
+    } catch(error) {
+        res.status(500).json({ message: 'Error retrieving the user'})
+    }
 });
 
 router.get('/:id/posts', (req, res) => {
