5da9b334166540782a6b46ab35dc43f0294aa00b
Brandi <Brandi@Brandis-iMac-4.local>
2019-08-10 12:02:25 -0400

    working server, routes initiated, dependencies added

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-const express = 'express';
+const express = require('express');
+const postRouter = require('./posts/postRouter.js')
+const userRouter = require('./users/userRouter.js')
+
+const helmet = require('helmet')
 
 const server = express();
+
+server.use(express.json())
+server.use(helmet())
+
+server.use('/api/posts', postRouter)
+server.use('/api/users', userRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
